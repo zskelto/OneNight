1cc3f31660c0ed253aa5141bc248774ee13ea4ec
Zachary <zskelto@g.clemson.edu>
2018-08-14 15:19:52 -0400

    modified:   main.py

--- main.py
+++ main.py
@@ -78,4 +78,13 @@ async def on_ready():
 	print(client.user.id)
 	print('------')
 
+async def list_servers():
+	await client.wait_until_ready()
+	while not client.is_closed:
+		print("Current Servers:")
+		for server in client.servers:
+			print(server.name)
+		await asyncio.sleep(600)
+
+client.loop.create_task(list_servers())
 client.run(cfg['Default']['token'])
